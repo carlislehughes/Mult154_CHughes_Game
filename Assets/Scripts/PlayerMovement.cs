541de0aaad7d8d7ad402e693932643fa858863e7
Carlisle Hughes <22939626+carlislehughes@users.noreply.github.com>
2021-10-17 19:32:06 -0700

    Changed player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    //private Rigidbody rbPlayer;
-   // private Vector3 direction = Vector3.zero;
     public float speed = 2.0f;
+    private Vector2 mInput = Vector2.zero;
+
+    public float mouseSensitivity = 1.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        //rbPlayer = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -20,17 +20,24 @@ public class PlayerMovement : MonoBehaviour
         float horMove = Input.GetAxis("Horizontal");
         float verMove = Input.GetAxis("Vertical");
 
+        //Mouse Inputs
+        mInput.y += Input.GetAxis("Mouse X");
+        mInput.x += -Input.GetAxis("Mouse Y");
+
 
         //moving
         transform.Translate(Vector3.forward * Time.deltaTime * speed * verMove);
-        //turning
-        transform.Rotate(Vector3.up, Time.deltaTime * 100.0f * horMove);
+        //strafing
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horMove);
 
-        //direction = new Vector3(horMove, 0, verMove);
+        //mouse look
+        mInput.x = Mathf.Clamp(mInput.x, -15f, 15f);
+        transform.eulerAngles = new Vector2(0, mInput.y) * mouseSensitivity;
+        Camera.main.transform.localRotation = Quaternion.Euler(mInput.x * mouseSensitivity, 0, 0);
+
+        /**
+         * source for mouse look: https://www.reddit.com/r/Unity3D/comments/8k7w7v/unity_simple_mouselook/
+         **/
+        
     }
-    //void FixedUpdate()
-    //{
-
-   //     rbPlayer.AddForce(direction * speed, ForceMode.Force);
-    //}
 }
