899a59df0b987b804b2419cab65518ae58e6effe
Carlisle Hughes <22939626+carlislehughes@users.noreply.github.com>
2021-10-25 22:53:58 -0700

    Animations

--- Assets/Scripts/PlayerInteractions.cs
+++ Assets/Scripts/PlayerInteractions.cs
@@ -9,9 +9,17 @@ public class PlayerInteractions : MonoBehaviour
     private AudioSource asPlayer;
     public AudioClip HeartBeatSound;
 
+    public GameObject[] serverPrefabs;
+
+
+    public Animator animTape;
+
+    private GameManager gameManager;
+
     // Start is called before the first frame update
     void Start()
     {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         shadow = GameObject.Find("Shadow").GetComponent<NavMovement>();
         asPlayer = GetComponent<AudioSource>();
     }
@@ -23,5 +31,55 @@ public class PlayerInteractions : MonoBehaviour
         {
             asPlayer.PlayOneShot(HeartBeatSound, .1f);
         }
+    }
+
+    void OnTriggerStay(Collider collider)
+    {
+        GameObject collidedWith = collider.gameObject;
+        //Interact with the tape player
+        if (collider.CompareTag("Tape Player"))
+        {
+            //Call for animations and audio as well as UI
+            gameManager.instructionsPopUp.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.E)) {
+                animTape.SetBool("StartStopTape", true);
+                StartCoroutine(VariableDelayStart());
+            }
+            else if (Input.GetKeyDown(KeyCode.R))
+            {
+                animTape.SetBool("Rewind", true);
+                StartCoroutine(VariableDelayRewind());
+
+            }
+            else if (Input.GetKeyDown(KeyCode.C))
+            {
+                animTape.SetBool("SwitchTape", true);
+                StartCoroutine(VariableDelaySwitch());
+            }
+
+            StartCoroutine(PopUpDelay());
+        }
+    }
+
+    IEnumerator PopUpDelay()
+    {
+        yield return new WaitForSeconds(2);
+        gameManager.instructionsPopUp.SetActive(false);
+    }
+
+    IEnumerator VariableDelayStart()
+    {
+        yield return new WaitForSeconds(15);
+        animTape.SetBool("StartStopTape", false);
+    }
+    IEnumerator VariableDelayRewind()
+    {
+        yield return new WaitForSeconds(4);
+        animTape.SetBool("Rewind", false);
+    }
+    IEnumerator VariableDelaySwitch()
+    {
+        yield return new WaitForSeconds(2);
+        animTape.SetBool("SwitchTape", false);
     }
 }
